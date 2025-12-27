d949b9480cd9527aa133b1e1781f2ab3097538df
ramkumar-2110 <ramkumar21102006@gmail.com>
2025-12-27 21:10:16 +0530

    Assesment 1

--- Assesment 1/prob17.c
+++ Assesment 1/prob17.c
@@ -6,6 +6,6 @@ int main()
     printf("Enter Number: ");
     scanf("%d",&x);
     y=x-5;
-    x=(y%2==1)?x:y; 
-    printf("Result = %d",x);
+    (x%2==1)&&printf("Result = %d",y);
+    (x%2!=1)&&printf("Result = %d",x);
 }
