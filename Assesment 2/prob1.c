60ac63ad32b41425d79a4828e17250fe12eef1fc
ramkumar-2110 <ramkumar21102006@gmail.com>
2026-01-05 11:05:49 +0530

    Decision making without if

--- Assesment 2/prob1.c
+++ Assesment 2/prob1.c
@@ -6,7 +6,8 @@ int main() {
     int x;
     printf("Enter the Number: ");
     scanf("%d", &x);
-    x == 50  && printf("Result = 1\n");
+    x == 50 && printf("Result = 1\n");
     x != 50 && printf("Result = 0\n");
     return 0;
 }
+                                                                         
